b8737a17afb84392dca21189a8ba3421c0b2694f
sachin21 <sachin21.developer@gmail.com>
2014-04-09 15:56:25 +0900

    remove newline character

--- lib/space2underscore.rb
+++ lib/space2underscore.rb
@@ -4,7 +4,7 @@ module Space2underscore
   def self.convert_to_underscore(argv)
     if argv
       space_include = argv.gsub(/\s/, '_')
-      copy_command   = `echo #{space_include} | pbcopy`
+      copy_command = `echo #{space_include} | ruby -ne 'print $_.chomp' | pbcopy`
       return space_include, copy_command
     else
       raise "Error! Please enter string"
