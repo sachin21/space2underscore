d59ab8bf117db4b3913ed5a39ea9b75d932bd390
Satoshi Ohmori <sachin21dev@gmail.com>
2014-04-18 00:19:10 +0900

    use system() method

--- lib/space2underscore.rb
+++ lib/space2underscore.rb
@@ -3,7 +3,7 @@ require 'space2underscore/version'
 module Space2underscore
   def self.convert_to_underscore(argv)
     space_include_sentence = argv.length == 1 ? argv.first.gsub(/\s/,  '_') : argv.join('_')
-    copy_command = `echo #{space_include_sentence} | ruby -pe 'chomp' | pbcopy`
+    copy_command = "echo #{space_include_sentence} | ruby -pe 'chomp' | pbcopy"
     return space_include_sentence, copy_command
   end
 end
