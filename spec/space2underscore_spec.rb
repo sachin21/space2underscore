a7cb2dea4fcbd8c2f7e2ad9c6ddf35f4ab5de144
sachin21 <sachin21.developer@gmail.com>
2014-04-20 19:10:17 +0900

    copy_command method only spec

--- spec/space2underscore_spec.rb
+++ spec/space2underscore_spec.rb
@@ -5,4 +5,8 @@ describe Space2underscore do
     expect(Space2underscore.convert(['fuga hoge foo'])).to eq('fuga_hoge_foo') # String case
     expect(Space2underscore.convert(%w(fuga hoge foo))).to eq('fuga_hoge_foo') # Array case
   end
+
+  it 'Successful copied' do
+    expect(Space2underscore.copy_command('fuga_hoge_foo')).to eq("echo fuga_hoge_foo | ruby -pe 'chomp' | pbcopy")
+  end
 end
