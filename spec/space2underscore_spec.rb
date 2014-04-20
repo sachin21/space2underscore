e16d635798f4d4f093bbae06ee59e2e16a999da2
sachin21 <sachin21.developer@gmail.com>
2014-04-20 19:00:51 +0900

    edit spec

--- spec/space2underscore_spec.rb
+++ spec/space2underscore_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Space2underscore do
   it 'Successful execution' do
-    expect(Space2underscore.convert_to_underscore(['fuga hoge foo']).first).to eq('fuga_hoge_foo') # String case
-    expect(Space2underscore.convert_to_underscore(%w(fuga hoge foo)).first).to eq('fuga_hoge_foo') # Array case
+    expect(Space2underscore.convert(['fuga hoge foo'])).to eq('fuga_hoge_foo') # String case
+    expect(Space2underscore.convert(%w(fuga hoge foo))).to eq('fuga_hoge_foo') # Array case
   end
 end
