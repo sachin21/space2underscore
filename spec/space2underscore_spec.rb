c1350ad899d7b74c0005fabc79486adc6a06147e
Satoshi Ohmori <sachin21dev@gmail.com>
2014-11-17 10:32:34 +0900

    Change matcher

--- spec/space2underscore_spec.rb
+++ spec/space2underscore_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Space2underscore do
   it 'Successful execution' do
-    expect(Space2underscore.convert(['fuga hoge foo'])).to eq('fuga_hoge_foo') # String case
-    expect(Space2underscore.convert(%w(fuga hoge foo))).to eq('fuga_hoge_foo') # Array case
+    expect(Space2underscore.convert(['fuga hoge foo'])).to include('_') # String case
+    expect(Space2underscore.convert(%w(fuga hoge foo))).to include('_') # Array case
   end
 
   it 'Successful copied' do
