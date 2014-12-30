04ffafd8b9b0533156eb551186693ce1e2dabda7
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-30 21:04:57 +0900

    Add copy command method

--- lib/space2underscore.rb
+++ lib/space2underscore.rb
@@ -7,7 +7,17 @@ module Space2underscore
     end
 
     def generate_command(space_include_sentence)
-      "echo #{space_include_sentence} | ruby -pe 'chomp' | pbcopy"
+      "echo #{space_include_sentence} | ruby -pe 'chomp' | #{copy_cmd}"
+    end
+
+    def copy_cmd
+      if system("type pbcopy > /dev/null 2>&1")
+        "pbcopy"
+      elsif system("type xsel > /dev/null 2>&1")
+        "xsel --input --clipboard"
+      elsif system("type xclip > /dev/null 2>&1")
+        "xclip"
+      end
     end
   end
 end
