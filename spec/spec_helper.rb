5236768d89a5d64e40da843214c572f6878d4848
Satoshi Ohmori <sachin21dev@gmail.com>
2015-08-27 17:58:26 +0900

    Remove needless config(s)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,7 @@ require 'space2underscore'
 Bundler.setup
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = 'random'
-
   config.expect_with :rspec do |rspec|
     rspec.syntax = :expect
   end
