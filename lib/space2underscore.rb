99e126dc2e237384a6ec9efbc17711759898d123
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-12 13:33:17 +0900

    Use self

--- lib/space2underscore.rb
+++ lib/space2underscore.rb
@@ -1,13 +1,11 @@
 require 'space2underscore/version'
 
 module Space2underscore
-  class << self
-    def convert(argv)
-      argv.length == 1 ? argv[0].strip.gsub(/\s/,  '_') : argv.join('_')
-    end
+  def self.convert(argv)
+    argv.length == 1 ? argv[0].strip.gsub(/\s/,  '_') : argv.join('_')
+  end
 
-    def create_new_branch(underscore_include_sentence)
-      system "git checkout -b #{underscore_include_sentence}"
-    end
+  def self.create_new_branch(underscore_include_sentence)
+    system "git checkout -b #{underscore_include_sentence}"
   end
 end
