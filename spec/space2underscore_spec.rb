7d2e8b3132ab404d812acc308a5af476ab8b2f1d
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-13 20:57:22 +0900

    Fix title in English

--- spec/space2underscore_spec.rb
+++ spec/space2underscore_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Space2underscore do
-  it 'should be Successful execution' do
+  it 'returns underscore included in string' do
     expect(Space2underscore.convert(['fuga hoge foo'])).to include('_') # String case
     expect(Space2underscore.convert(%w(fuga hoge foo))).to include('_') # Array case
   end
