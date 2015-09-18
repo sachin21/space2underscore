1a0b26f176011eafa58bc93f3faaf18a6c8eddf2
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-18 17:06:15 +0900

    Add usage

--- lib/space2underscore.rb
+++ lib/space2underscore.rb
@@ -8,4 +8,20 @@ module Space2underscore
   def self.create_new_branch(underscore_include_sentence)
     system "git checkout -b #{underscore_include_sentence}"
   end
+
+  def self.print_usage
+    message = <<-EOF
+      space2underscore #{Space2underscore::VERSION}
+      Usage: space2underscore <branch name> <options>
+
+      $ space2underscore new branch -c # Run with `--create` or `-c` options.
+      =>  Switched to the new branch 'new_branch’
+
+      Or
+
+      $ git branch -m $(space2underscore renamed branch) # space2underscore has outputted to the standard output.
+    EOF
+
+    print message.gsub('      ', '')
+  end
 end
