28e27fbd1f9de63cb7c1ef1e59cc7808b7f0d991
Satoshi Ohmori <sachin21dev@gmail.com>
2015-10-20 04:54:10 +0900

    Configure of gems for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,19 @@
 require 'bundler/setup'
+require 'coveralls'
+Coveralls.wear!
+
 require 'space2underscore'
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/vendor/'
+end
+
 Bundler.setup
 
 RSpec.configure do |config|
