4fb1f71f1a9f792ed91491ba52acba18629882dd
Satoshi Ohmori <sachin21dev@gmail.com>
2015-10-19 15:33:23 +0900

    Write spec for .create_new_branch

--- spec/space2underscore_spec.rb
+++ spec/space2underscore_spec.rb
@@ -1,8 +1,44 @@
 require 'spec_helper'
 
 describe Space2underscore do
-  it 'returns underscore included in string' do
-    expect(Space2underscore.convert(['fuga hoge foo'])).to include('_') # String case
-    expect(Space2underscore.convert(%w(fuga hoge foo))).to include('_') # Array case
+  let(:branch_name) { 'foo_bar' }
+  let(:hidden) { '> /dev/null 2>&1'}
+
+  describe '.convert' do
+    it 'returns underscore included in string' do
+      expect(Space2underscore.convert(['foo bar'])).to include(branch_name)
+      expect(Space2underscore.convert(%w(foo bar))).to include(branch_name)
+    end
+  end
+
+  def checkout_and_remove_branch
+    checkout_master = "git checkout master #{hidden}"
+    delete_branch = "git branch -D #{branch_name} #{hidden}"
+
+    system("#{checkout_master}; #{delete_branch}")
+  end
+
+  def create_branch
+    system("git branch #{branch_name} #{hidden}")
+  end
+
+  describe '.create_new_branch' do
+    after { checkout_and_remove_branch }
+
+    context "when exist a git repository" do
+      before { checkout_and_remove_branch }
+
+      it 'creates the new branch' do
+        expect(Space2underscore.create_new_branch(branch_name)).to be_truthy
+      end
+    end
+
+    context "when exist a git repository" do
+      before { create_branch }
+
+      it 'creates the new branch' do
+        expect(Space2underscore.create_new_branch(branch_name)).to be_falsey
+      end
+    end
   end
 end
